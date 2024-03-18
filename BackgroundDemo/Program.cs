c13c81549f0e9c01801aedf47047962c78e9d556
sheng <chrisxin911@hotmail.com>
2024-03-18 10:47:04 -0400

    add service and endpoint

--- BackgroundDemo/Program.cs
+++ BackgroundDemo/Program.cs
@@ -4,7 +4,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//add data.
 builder.Services.AddSingleton<SampleData>();
+//add background service.
+builder.Services.AddHostedService<BackgroundRefresh>();
 
 var app = builder.Build();
 
@@ -15,6 +18,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//endpoint
+app.MapGet("/message", (SampleData data) => data.Data.Order());
 
 app.Run();
 
